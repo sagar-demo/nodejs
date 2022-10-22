3cfa7fc2b346a0cf6c6e117de0891c58cd75b9a1
Rana  Sagar <64120304+sagar-demo@users.noreply.github.com>
2022-10-22 17:12:57 +0530

    Add files via upload

--- index2.js
+++ index2.js
@@ -1,10 +1,22 @@
 const http=require('http');
 const port =100;
+const fs=require('fs');
+
 function requestHandler(req,res){
     console.log(req.url);
     res.writeHead(200,{'content-type':'text/html'});
-    res.end("<h1>hello sagar</h1>")
+    fs.readFile('./index.html',function(err,data){
+        if(err){
+            console.log('error',err);
+            return res.end('<h1>Error!</h1>');
+        }
+        return res.end(data);
+        
+    
+    });
+    
 };
+
 const server=http.createServer(requestHandler);
 server.listen(port,function(err){
     if(err){
